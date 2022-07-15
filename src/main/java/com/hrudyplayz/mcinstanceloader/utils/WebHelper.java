50535072f7d16b8976eb69aedb15427816d57cb4
HRudyPlayZ <rudy.hebrard@gmail.com>
2022-07-15 21:02:33 +0200

    Pushed release 2.1
    - Rewrote the WebHelper class again
    - Fixed a crash linked to the Apache HTTP library missing on servers for some dumb reasons
    - Fixed an error that could happen when following a link with a URL and not just a file name.
    - Removed accidental debug print leftovers while loading the config
    - Removed the empty URL follow error. (It was useless anyways)

--- src/main/java/com/hrudyplayz/mcinstanceloader/utils/WebHelper.java
+++ src/main/java/com/hrudyplayz/mcinstanceloader/utils/WebHelper.java
@@ -1,27 +1,19 @@
 package com.hrudyplayz.mcinstanceloader.utils;
 
-import com.hrudyplayz.mcinstanceloader.Config;
-import com.hrudyplayz.mcinstanceloader.Main;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.client.LaxRedirectStrategy;
-
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.*;
 import java.util.regex.Pattern;
+
+import org.apache.commons.io.FileUtils;
+
+import com.hrudyplayz.mcinstanceloader.Config;
+import com.hrudyplayz.mcinstanceloader.Main;
 
 
 @SuppressWarnings("unused")
 public class WebHelper {
 // This class will allow to download a file from the internet.
-// Most of it is adapted from https://github.com/Janrupf/mod-director/tree/master/mod-director-core/src/main/java/net/jan/moddirector/core/util.
-// Shoutout to both Janrupf and HansWasser for making it in the first place.
 
     // Defines the client properties, uses Twitch UserAgents to make every website work correctly as they should.
     public static String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) twitch-desktop-electron-platform/1.0.0 Chrome/73.0.3683.121 Electron/5.0.12 Safari/537.36 desklight/8.51.0";
@@ -30,44 +22,42 @@ public class WebHelper {
     public static boolean downloadFile (String fileURL, String savePath) {
     // Lets you download a file from a specific URL and save it to a location.
 
-        RequestConfig timeouts = RequestConfig.custom() // Creates the request config to set the custom timeout values
-                                              .setConnectTimeout(Config.connectionTimeout * 1000)
-                                              .setConnectionRequestTimeout(Config.connectionTimeout * 1000)
-                                              .setSocketTimeout(Config.connectionTimeout * 1000)
-                                              .build();
-
-        CloseableHttpClient client = HttpClients.custom() // Creates an httpClient with custom properties
-                                                .setUserAgent(USER_AGENT) // Adds the user-agent
-                                                .setRedirectStrategy(new LaxRedirectStrategy()) // Allows for redirect handling
-                                                .setDefaultRequestConfig(timeouts)
-                                                .disableContentCompression().build(); // Disables compression (slightly faster and more reliable downloads) and builds the client.
-
+        URL url;
         try {
-            HttpGet request = new HttpGet(new URL(fileURL).toURI()); // Creates the GET request.
-            request.addHeader("Referer", REFERER); // Adds the referer to the request.
-
-            if (Config.curseforgeAPIKey.length() > 0) request.addHeader("x-api-key", Config.curseforgeAPIKey);
-
-            HttpEntity entity = client.execute(request).getEntity();
-            if (entity != null) FileUtils.copyInputStreamToFile(entity.getContent(), new File(savePath));
-
-            return true;
+            url = new URL(fileURL);
         }
-        catch (URISyntaxException e) {
+        catch (Exception e) {
             Main.errorContext = "The URL is invalid.";
             return false;
         }
-        catch (ClientProtocolException e) {
-            Main.errorContext = "HTTP Protocol violation.";
-            return false;
+
+        try {
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setInstanceFollowRedirects(true);
+
+            connection.setReadTimeout(Config.connectionTimeout * 1000);
+            connection.setConnectTimeout(Config.connectionTimeout * 1000);
+
+            connection.setRequestProperty("User-Agent", USER_AGENT);
+            connection.setRequestProperty("Referer", REFERER);
+            if (Config.curseforgeAPIKey.length() > 0) connection.setRequestProperty("x-api-key", Config.curseforgeAPIKey);
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                Main.errorContext = "Received an HTTP " + responseCode + " error.";
+                return false;
+            }
+
+            File file = new File(savePath);
+            FileUtils.copyInputStreamToFile(connection.getInputStream(), file);
+            return true;
         }
         catch (IOException e) {
             Main.errorContext = "There was an issue writing to file.";
             return false;
         }
-        finally {
-            IOUtils.closeQuietly(client);
-        }
+
     }
 
 
@@ -86,9 +76,7 @@ public class WebHelper {
             String html = "";
             for (String s : lines) html += s;
 
-            String matched = ">" + follows[counter] + "<";
-
-            // Yeah i parse HTML with regex, what are you gonna do? Kill me? x)
+            // This parses the HTML file using regex.
             // Thanks to the Melody language for letting us generate regex using a readable syntax :P
             String[] splitted = html.split(">\\s*" + Pattern.quote(follows[counter]) + "\\s*<");
             if (splitted.length == 1) {
@@ -101,16 +89,14 @@ public class WebHelper {
             href = href.substring(0, href.indexOf("\""));
 
             try {
-                URL url = new URL(fileURL);
-                fileURL = url.getProtocol() + "://" + url.getHost() + "/" + href;
+                if (href.startsWith("http://") || href.startsWith("https://")) fileURL = href;
+                else {
+                    URL url = new URL(fileURL);
+                    fileURL = url.getProtocol() + "://" + url.getHost() + "/" + href;
+                }
             }
             catch (Exception e) {
                 Main.errorContext = "Following " + follows[counter] + " lead to an invalid URL.";
-                return false;
-            }
-
-            if (fileURL.isEmpty()) {
-                Main.errorContext = "Result URL was empty when following " + follows[counter] + ".";
                 return false;
             }
 
